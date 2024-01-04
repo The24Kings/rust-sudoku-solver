eeab2ad5529ac231ab87bb720257e76bbe8616ba
24Kings <24kingsunite@gmail.com>
2024-01-04 06:29:47 -0800

    added sudoku rule checking (untested)

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -17,7 +17,19 @@ impl Board {
             board: new_board,
         }
     }
+/*
+    pub fn coords(index: usize) -> [u16;2] {
+        [(index%9),(index/9)] // x, y
+    }
 
+    pub fn x(%self, index: usize) - u16 {
+        index % 9
+    }
+    
+    pub fn y(%self, index: usize) - u16 {
+        index / 9
+    }
+*/
     pub fn cell(&mut self, index: usize) -> &mut State {
         &mut self.board[index]
     }
@@ -26,5 +38,76 @@ impl Board {
         self.board[index].active = num;
     }
 
+    pub fn row(&self, index: usize) -> Vec<State> {
+        let start = index % 9;
+        let end = start + 9;
+
+        self.board[start..end]
+    }
+
+    pub fn col(&self, index: usize) -> Vec<State> {
+        let start = index / 9;
+
+        let mut output: Vec<State> = vec!();
+
+        for _i in 0..9 {
+            output.push(self.board[start + (_i * 9)]);
+        }
+
+        output
+    }
+
+    pub fn group(&self, index: usize) -> Vec<State> {
+        let mut output: Vec<State> = vec!();
+        let offset = index % 3;
+
+        let mut corner = index - offset - (9 * offest);
+
+        for _i in 0..9 {
+            if _i / 3 == 0 && _i != 0 {
+                corner += 9;
+            }
+
+            output.push(self.board[corner]);
+            
+            corner += 1;
+        }
+
+        output
+    }
+
+    pub check(&self, num: u16, index: usize) -> bool {
+        check_row(num, index) && check_col(num, index) && check_box(num, index)
+    }
+
+    pub fn check_row(&self, num: u16, index: usize) -> bool {
+        for element in self.row(index) {
+            if num == element.active() {
+                false
+            }
+        }
+
+        true
+    }
+
+    pub fn check_col(&self, num: u16, index: usize) -> bool {
+        for element in self.col(index) {
+            if num == element.active() {
+                false
+            }
+        }
+
+        true 
+    }
+
+    pub fn check_box(&self, num: u16, index: usize) -> bool {
+        for element in self.group(index) {
+            if num == element.active() {
+                false
+            }
+        } 
+        
+        true
+    }
 }
 
