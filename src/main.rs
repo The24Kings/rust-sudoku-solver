1081f27f24e58da055b8ab857b49c91c3316ff02
24Kings <24kingsunite@gmail.com>
2024-01-05 00:02:45 -0800

    basic testing

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,45 @@ mod board;
 mod stack;
 
 fn main() {
+    let mut game = Board::new();
+
+    // Mock setup for testing
+    game.confirm(1, 8);
+    game.confirm(7, 3);
+    game.confirm(9, 2);
+    game.confirm(12, 6);
+    game.confirm(14, 7);
+    game.confirm(17, 1);
+    game.confirm(28, 6);
+    game.confirm(30, 2);
+    game.confirm(32, 1);
+    game.confirm(34, 7);
+    game.confirm(36, 5);
+    game.confirm(44, 3);
+    game.confirm(45, 9);
+    game.confirm(48, 7);
+    game.confirm(50, 5);
+    game.confirm(53, 8);
+    game.confirm(54, 4);
+    game.confirm(56, 1);
+    game.confirm(57, 3);
+    game.confirm(59, 9);
+    game.confirm(60, 7);
+    game.confirm(62, 6);
+    game.confirm(64, 2);
+    game.confirm(70, 1);
+    game.confirm(72, 8);
+    game.confirm(74, 3);
+    game.confirm(75, 1);
+    game.confirm(77, 6);
+    game.confirm(78, 5);
+    game.confirm(80, 9);
+
+    // Testing
+/*
     let mut board = Board::new();
     let mut cell = State::new();
-    let mut guesses: Stack<usize> = Stack::new(); // Stores the index of each State that we have already activated and guessed
+    let mut guesses: Stack<usize> = Stack::new(); // Stores the index of each State that we have already confirmd and guessed
 
     println!("{}\n",cell.active());
 
@@ -33,7 +69,7 @@ fn main() {
     }
     */
 
-    board.cell(1).activate(3);
+    board.cell(1).confirm(3);
     board.activate(2,4);
 
     println!("\n{}",board.cell(1).active());
@@ -51,5 +87,5 @@ fn main() {
     println!("Last: {}",guesses.peek().unwrap());
     
     println!("\n{:?}",guesses);
-
+*/
 }
